41a61f698d582f4b838d2b616111dbebb03942c4
Jingjing Fang <59980749+jingjingfang@users.noreply.github.com>
2020-08-13 21:12:15 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function ageInDays(){
+    var birthYear =prompt("Which year were you born?");
+    var currentTime=new Date();
+    var currentYear=currentTime.getFullYear();
+    var days=(currentYear-birthYear)*365;
+ 
+    //create a new h1 element
+    var h1 =document.createElement('h1');
+   // create a content
+    var answer=document.createTextNode("You have lived "+ days +" days.");
+ 
+    /*set the id attribute to "age" to a h1 element:
+    the id is flex-box-reault  
+    Element.setAttribute(attribute, value);   
+    */
+    h1.setAttribute("id","age");  
+    //add a text note to h1
+    h1.appendChild(answer);
+ 
+    //add h1 value to the "flex-box-result"
+    document.getElementById("flex-box-result").appendChild(h1);
+ }
+ 
+ 
+ function reset(){
+     //remove the 
+     document.getElementById("age").remove();
+ }
