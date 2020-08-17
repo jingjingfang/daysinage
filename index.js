c33a8daafd162727e52350f04610b9eeabd9d1f5
Jingjing Fang <59980749+jingjingfang@users.noreply.github.com>
2020-08-17 11:50:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-function ageInDays(){
-    var birthYear =prompt("Which year were you born?");
-    var currentTime=new Date();
-    var currentYear=currentTime.getFullYear();
-    var days=(currentYear-birthYear)*365;
- 
-    //create a new h1 element
-    var h1 =document.createElement('h1');
-   // create a content
-    var answer=document.createTextNode("You have lived "+ days +" days.");
- 
-    //set the id attribute to "age" to a h1 element   
-         h1.setAttribute("id","age");  
-    //put a text note into h1
-    h1.appendChild(answer);
- 
-    //add h1 value to the "flex-box-result"
-    document.getElementById("flex-box-result").appendChild(h1);
- }
- 
- 
- function reset(){
-     //remove the 
-     document.getElementById("age").remove();
- }
+function ageInDays() {
+  let birthYear = prompt('Which year were you born?');
+  let currentTime = new Date();
+  let currentYear = currentTime.getFullYear();
+  let days = (currentYear - birthYear) * 365;
+
+  // create a text node
+  let answer = document.createTextNode('You have lived ' + days + ' days.');
+
+  //create a new h1 element <h1>
+  let h1 = document.createElement('h1');
+
+  //puts a text note into h1
+  h1.appendChild(answer);
+
+  //add h1 value to the "flex-box-result"
+  document.getElementById('flex-box-result').appendChild(h1);
+
+  //set the id attribute to "age" to a h1 element
+  h1.setAttribute('id', 'age');
+}
+
+function reset() {
+
+  document.getElementById('age').remove();
+}
